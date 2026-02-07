ca73d861dd906a97cd7c94d6124430a777945f67
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-02-07 19:17:18 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import Nav from "./Components/Nav/Nav"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 
 
 function App() {
-
   return (
-
     <>
+      <BrowserRouter>
 
+        <Nav />
 
+        <Routes>
+        </Routes>
 
+      </BrowserRouter>
     </>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,1 +1,46 @@
+import './Nav.css'
 
+
+
+export default function Nav() {
+
+    return (
+        <>
+
+            <nav>
+                <div>
+                    <div>
+                        <img src="src\Images\image.png" alt="" />
+
+                    </div>
+
+                    <div className='logo'>
+
+                    </div>
+
+                </div>
+
+
+                <div>
+                    <div className='imaxBlock'>
+                        <img src="src\Images\logo_imax_white.png" alt="" />
+                    </div>
+
+                    <div >
+                        Львів
+                        <span>&#9650;</span>
+                    </div>
+
+
+                    <div>
+                        Увійти
+                    </div>
+
+                </div>
+            </nav>
+
+
+
+        </>
+    )
+}
