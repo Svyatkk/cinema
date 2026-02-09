1a521fb26c97e74616a94900d8305d51c0dd39d5
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-02-09 21:31:53 +0200

    update

--- crud-backend/index.js
+++ crud-backend/index.js
@@ -1,26 +1,23 @@
-import cors from 'cors'
 import express from 'express'
-
-
+import cors from 'cors'
+import { films } from './films.js'
 
 const app = express()
-
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
 
-
-
-app.get('/', (req, res) => {
-
-    res.send('its ok')
-
-
+app.get('/films', (req, res) => {
+    res.json(films)
 })
 
+app.get('/films/:id', (req, res) => {
+    const film = films.find(f => f.id === Number(req.params.id))
+    res.json(film)
+})
 
 app.listen(port, () => {
-
-    console.log('port is running')
+    console.log('Server running on port 3000')
 })
+
